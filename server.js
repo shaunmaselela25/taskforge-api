2298e5aa6d012f9de84913f540a0db2e13ead87e
shaunmaselela25 <160026940+shaunmaselela25@users.noreply.github.com>
2026-08-16 16:12:45 +0000

    feat: added global request logger

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+
+const logger = require("./middleware/logger");
+
 const app = express ();
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(logger);
 
 app.listen(PORT, () => {
     console.log(`TaskForge API running on port ${PORT}`);
