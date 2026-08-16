4ae1154630dbba4001aab8a1a7038e38d181bf41
shaunmaselela25 <160026940+shaunmaselela25@users.noreply.github.com>
2026-08-16 19:34:50 +0000

    feat: implemented task by id

--- routes/tasks.js
+++ routes/tasks.js
@@ -30,4 +30,16 @@ router.get("/", (req, res) => {
     res.status(200).json(tasks);
 });
 
+router.get("/:id", (req, res) => {
+    const task = tasks.find((task) => task.id === req.params.id);
+
+    if(!task) {
+        return res.status(404).json({
+            error: "Task not found",
+        });
+    }
+
+    res.status(200).json(task);
+});
+
 module.exports = router;
