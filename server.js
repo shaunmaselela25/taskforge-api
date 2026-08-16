e38b3cc88cb3a1484814fedcd103bcdad387e114
shaunmaselela25 <160026940+shaunmaselela25@users.noreply.github.com>
2026-08-16 17:55:41 +0000

    feat: add  in-memory task store

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const logger = require("./middleware/logger");
+const taskRoutes = require("./routes/tasks");
 
 const app = express ();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(logger);
+
+app.use("/tasks", taskRoutes);
 
 app.listen(PORT, () => {
     console.log(`TaskForge API running on port ${PORT}`);
