dd5c6f17bf420f86ed63f9a61b7ccb3f7d8e276f
shaunmaselela25 <160026940+shaunmaselela25@users.noreply.github.com>
2026-08-17 08:29:34 +0000

    feat: implemented POST tasks endpoint

--- routes/tasks.js
+++ routes/tasks.js
@@ -42,4 +42,26 @@ router.get("/:id", (req, res) => {
     res.status(200).json(task);
 });
 
+router.post("/", (req, res) => {
+    const { title } = req.body;
+
+    if(!title) {
+        return res.status(400).json({
+            error: "Title is required",
+        });
+
+    }
+
+    const task = {
+        id: nanoid(),
+        title,
+        complete: false,
+        createdAt: new Date().toISOString(),
+    };
+
+    tasks.push(task);
+
+    res.status(201).json(task);
+});
+
 module.exports = router;
